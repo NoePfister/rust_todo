60b58830e340ccf4694420f64a92fa94c7a4bc90
NoePfister <noe.pfister@gmx.ch>
2024-06-03 06:47:49 +0200

    alsmost done

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::{io,process};
+use std::{fs, io, process};
 
 fn main() {
+    let mut app = App::new();
+    app.load_todos();
+
     'command: loop {
-        clear();
+        App::clear();
+
+
+
         println!("TODO Manager!");
         println!("Please enter your command:");
         println!("q -> Quit application");
         println!("s -> Show Todo's");
         println!("n -> New Todo");
+        println!("d -> Mark Todo as done");
 
         let mut command = String::new();
 
@@ -22,9 +29,9 @@ fn main() {
         command = command.trim().to_lowercase();
 
         match command.as_str() {
-            "q" => quit(),
-            "s" => show(),
-            "n" => new(),
+            "q" => app.quit(),
+            "s" => app.show(),
+            "n" => app.new_todo(),
             _ => {
                 println!("Invalid command");
                 continue 'command;
@@ -33,14 +40,83 @@ fn main() {
     }
 }
 
-fn quit() {
-    process::exit(0);
+struct App {
+    todos: Vec<String>,
+    save_path: String,
 }
 
-fn show() {}
+impl App {
+    fn new() -> App {
+        App {
+            todos: Vec::new(),
+            save_path: "/rust_todo.txt".to_string(),
+        }
+    }
 
-fn new() {}
+    fn quit(&mut self) {
+        process::exit(0);
+    }
 
-fn clear() {
-    print!("{}[2J", 27 as char);
+    fn show(&mut self) {
+        App::clear();
+        println!("Todos:");
+        // print todo's
+        for (i,todo) in self.todos.iter().enumerate() {
+            let j = 1+i;
+            println!("{j}. {}", todo);
+        }
+
+        //wait for key press:
+        println!("Press enter to continue tu menu");
+        App::wait_for_input();
+
+
+    }
+
+    fn new_todo(&mut self) {
+        App::clear();
+        println!("Please input your new Todo:");
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(_) => {
+                println!("Couldn't read command line!");
+            }
+        }        
+
+        self.todos.push(input.trim().to_string());
+
+    }
+
+    fn load_todos(&mut self) {
+
+
+        let save_string = fs::read_to_string(&self.save_path).expect("Couldn't read save file");    
+
+        println!("{}",save_string);
+    }
+
+    fn save_todos(&mut self) {
+        let mut save_string = String::new();
+        for todo in self.todos.iter(){
+            save_string += todo;
+        }
+
+        fs::write(&self.save_path,save_string).expect("Couldn't write to file!")
+    }
+
+    fn clear() {
+        print!("{}[2J", 27 as char);
+    }
+
+    fn wait_for_input() {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(_) => {
+                println!("Couldn't read command line!");
+            }
+        }
+    }
 }
